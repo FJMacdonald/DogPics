920815efe19bff6778f8d7b28f8eaaf0179e616e
Francesca Macdonald <104513846+Francesca-Quevita@users.noreply.github.com>
2023-08-28 14:24:09 -0400

    Initial Commit

---
+++ DogPics/DogPicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogPicsApp.swift
+//  DogPics
+//
+//  Created by Francesca MACDONALD on 2023-08-28.
+//
+
+import SwiftUI
+
+@main
+struct DogPicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
